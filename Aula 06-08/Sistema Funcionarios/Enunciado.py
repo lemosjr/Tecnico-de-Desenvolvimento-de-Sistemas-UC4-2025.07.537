286de364c6c93301e292b7ef85402a0d2a0a459d
lemosjr <160804009+lemosjr@users.noreply.github.com>
2025-08-06 20:56:25 -0300

    Update Enunciado.py

--- Aula 06-08/Sistema Funcionarios/Enunciado.py
+++ Aula 06-08/Sistema Funcionarios/Enunciado.py
@@ -1,6 +1,10 @@
-# 1. Crie um sistema de gerenciamento de funcionários que faz uso de Programação Orientada a Objetos. Você deve criar um menu de opções que contém as opções cadastrar funcionário e listar funcionários. Ao cadastrar funcionário, você deve utilizar a Classe Funcionario e armazenar o funcionário criado em uma lista. Ao listar funcionários, você deve exibir os nomes e salários de todos os funcionários cadastrados.
+# 1. Crie um sistema de gerenciamento de funcionários que faz uso de Programação Orientada a Objetos.
+# Você deve criar um menu de opções que contém as opções cadastrar funcionário e listar funcionários.
+# Ao cadastrar funcionário, você deve utilizar a Classe Funcionario e armazenar o funcionário criado em uma lista.
+# Ao listar funcionários, você deve exibir os nomes e salários de todos os funcionários cadastrados.
 # Dados do Funcionário:
 # - Nome
 # - Salário
 # - Cargo
-# Bônus: Crie um método para exibir os dados de um funionário específico e permita que o usuário escolha qual funcionário deseja visualizar. Crie um método para dar aumento para todos os funcionários.
+# Bônus: Crie um método para exibir os dados de um funionário específico e permita que o usuário escolha qual
+# funcionário deseja visualizar. Crie um método para dar aumento para todos os funcionários.
